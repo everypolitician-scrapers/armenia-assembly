fe4959147f5d8c86171e257f37037ffa456c2d8b
Tony Bowden <tony@tmtm.com>
2017-01-05 11:55:28 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -10,7 +11,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -19,12 +20,12 @@ def noko_for(url)
 end
 
 def faction_from(text)
-  return ['Republican', 'RPA'] if text == '"Republican" (RPA) Faction'
+  return %w(Republican RPA) if text == '"Republican" (RPA) Faction'
   return ['Prosperous Armenia', 'PA'] if text == '"Prosperous Armenia" Faction'
-  return ['Heritage', 'H'] if text == '"Heritage" Faction'
+  return %w(Heritage H) if text == '"Heritage" Faction'
   return ['Armenian Revolutionary Federation', 'ARF'] if text == '"Armenian Revolutionary Federation" Faction'
   return ['Rule of Law', 'ROL'] if text == '"Rule of Law" Faction'
-  return ['Independent', '_IND'] if text == 'Not included'
+  return %w(Independent _IND) if text == 'Not included'
   return ['Armenian National Congress', 'ANC'] if text == '"Armenian National Congress" Faction'
   raise "unknown faction: #{text}"
 end
@@ -40,17 +41,17 @@ end
 def scrape_person(url)
   noko = noko_for(url)
   box = noko.css('.dep_description')
-  data = { 
-    id: url.to_s[/ID=(\d+)/, 1],
-    name: noko.css('.dep_name').text.tidy,
-    role: noko.css('.dep_position').text.tidy,
-    image: noko.css('img.dep_pic/@src').text,
-    district: box.xpath('//td[div[text()="District"]]/following-sibling::td').text,
-    party: box.xpath('//td[div[text()="Party"]]/following-sibling::td').text,
-    birth_date: box.xpath('//td[div[text()="Birth date"]]/following-sibling::td').text.split('.').reverse.join("-"),
-    email: box.css('a[href*="mailto:"]').text,
-    term: 5,
-    source: url.to_s,
+  data = {
+    id:         url.to_s[/ID=(\d+)/, 1],
+    name:       noko.css('.dep_name').text.tidy,
+    role:       noko.css('.dep_position').text.tidy,
+    image:      noko.css('img.dep_pic/@src').text,
+    district:   box.xpath('//td[div[text()="District"]]/following-sibling::td').text,
+    party:      box.xpath('//td[div[text()="Party"]]/following-sibling::td').text,
+    birth_date: box.xpath('//td[div[text()="Birth date"]]/following-sibling::td').text.split('.').reverse.join('-'),
+    email:      box.css('a[href*="mailto:"]').text,
+    term:       5,
+    source:     url.to_s,
   }
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
 
@@ -62,18 +63,18 @@ def scrape_person(url)
   noko_ru = noko_for(url_ru)
   data[:name__ru] = noko_ru.css('.dep_name').text.tidy
 
-  factions = box.xpath('//td[div[text()="Factions"]]/following-sibling::td//table//td').reject { |n| n.text.tidy.empty? }.map { |f|
+  factions = box.xpath('//td[div[text()="Factions"]]/following-sibling::td//table//td').reject { |n| n.text.tidy.empty? }.map do |f|
     start_date, end_date = f.css('span').text.split(' - ').map { |d| d.split('.').reverse.join('-') }
     faction, faction_id = faction_from f.css('a').text
     {
       faction_id: faction_id,
-      faction: faction,
+      faction:    faction,
       start_date: start_date,
-      end_date: end_date,
+      end_date:   end_date,
     }
-  }
+  end
   factions.each do |f|
-    ScraperWiki.save_sqlite([:id, :term, :start_date], data.merge(f))
+    ScraperWiki.save_sqlite(%i(id term start_date), data.merge(f))
   end
 end
 
